bb9723feb6b46a4c129424f195e1adc6311465dc
Mona-Eldouse <99315452+Mona-Eldouse@users.noreply.github.com>
2022-04-04 23:39:14 +0300

    blaaaa

---
+++ Ramadan.js
@@ -0,0 +1,19 @@
+alert("hello from Ramadan page ")
+
+// inputs 
+ let butt1 = document.getElementById("but1")
+
+
+
+// addEvent 
+ butt1.addEventListener("click" , show) 
+
+
+
+//  functions
+
+function show(){
+    alert("hiii")
+    let showdiv = document.getElementById("style1")
+    showdiv.style.display ="inline"
+}
