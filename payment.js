99a98c0e55b075a8707ab14b0d4e64baae791fcc
Mona-Eldouse <99315452+Mona-Eldouse@users.noreply.github.com>
2022-04-13 10:28:29 +0300

    tr

--- payment.js
+++ payment.js
@@ -6,11 +6,11 @@ let check = document.getElementById("btn");
 check.addEventListener("click", save);
 
 function save() {
-  let cname = document.getElementById("cname");
-  let ccnum = document.getElementById("ccnum");
-  let expmonth = document.getElementById("expmonth");
-  let expyear = document.getElementById("expyear");
-  let cvv = document.getElementById("cvv");
+  let cname = document.getElementById("cname").value;
+  let ccnum = document.getElementById("ccnum").value;
+  let expmonth = document.getElementById("expmonth").value;
+  let expyear = document.getElementById("expyear").value;
+  let cvv = document.getElementById("cvv").value;
 
   if (
     cname == "" ||
